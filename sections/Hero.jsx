453934e5fdc85089cab13e221a6961cc9d288686
justhappyuknow <manpari1979@gmail.com>
2024-10-21 21:33:14 +0530

    gif cahnged

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -155,7 +155,7 @@ function Hero() {
         >
           <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
           <img
-            src="/gif3.gif"
+            src="/gif4.gif"
             alt="cover"
             className="w-full sm:h-[500px] h-[500px] lg:h-[500px] object-cover rounded-tl-[140px] z-10 relative"
           />
